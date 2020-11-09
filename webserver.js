81f2422344ae9d13c2bf7293b696a63f42236409
E. Almqvist <elalmqvist@gmail.com>
2020-11-09 23:16:24 +0100

    Added systemd service & webserver tweaks

--- webserver.js
+++ webserver.js
@@ -12,15 +12,15 @@ server.use( express.static(clientdir) ); // all of the shared client files
 
 server.get("/", function(req, res) { // root directory for the website
 	var curdate = new Date();
-	var debug = "[" + curdate.toISOString() + "] HTTP Get: " + req.connection.remoteAddress; // create a string for the debug
+	var debugstr = "[" + curdate.toISOString() + "] HTTP Get: " + req.connection.remoteAddress; // create a string for the debug
 
-	fs.appendFile("logs/connections.log", debug + "\n", function(err){ if(err) throw err; }); // append the logs
-	console.log(debug);
-	res.sendFile(__dirname + "/index.html"); // load index.html
+	fs.appendFile("logs/connections.log", debugstr + "\n", function(err){ if(err) throw err; }); // append the logs
+	console.log(debugstr);
+	res.sendFile(clientdir + "/index.html"); // respond with the index.html file
 });
 
 server.get("*", function(req, res) {
-	res.sendFile(__dirname + "/404.html");
+	res.sendFile(clientdir + "/404.html");
 });
 
 var http_webserver = http.createServer(app);
